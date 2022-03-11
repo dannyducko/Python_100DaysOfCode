da41571dd1694413e551a036a590d6b25a324771
dannyducko <danny@ducko.uk>
2022-03-11 16:16:54 +0000

    codeacademy course updates

---
+++ Day6_Functions_and_Karel/defining_and_calling_functions.py
@@ -0,0 +1,34 @@
+# Built in functions
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+#########################
+
+# def creates, or defines a function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+# calling the function
+my_function()
+
+#####################
+# Reeborg.ca solution
+# def jump_hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#
+#
+# for steps in range(6):
+#     jump_hurdle()
---
+++ Day6_Functions_and_Karel/while_loops.py
@@ -0,0 +1,26 @@
+## For Loops
+# for item in list_of_items:
+# do something to each item
+
+# for number in range(a, b):
+#     print(number)
+
+# Use for loops when iterating through something, like a list
+#################################
+## While Loops
+# while something_is_true:
+# do something repeatedly.
+
+
+# while not at_goal():
+#     jump()
+#################################
+def hello():
+    number_of_hello = 6
+    while number_of_hello > 0:
+        print("Hello!")
+        number_of_hello -= 1
+        print(number_of_hello)
+
+
+hello()
